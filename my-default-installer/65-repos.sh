6ff59011b796268646c562a7969abd1b8505121d
Alan <alanfromjapan@gmail.com>
2021-10-08 22:18:23 +0900

    Added the repos

---
+++ my-default-installer/65-repos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p ~/Git
+
+git clone https://github.com/AlanFromJapan/alanarduinotools.git ~/Git/alanarduinotools
+git clone https://github.com/AlanFromJapan/alanWebSites.git ~/Git/alanWebSites
+git clone https://github.com/AlanFromJapan/Gameboy.git ~/Git/Gameboy
+git clone https://github.com/AlanFromJapan/pcb-design.git ~/Git/pcb-design
+git clone https://github.com/AlanFromJapan/linux-experiments.git ~/Git/linux-experiments
+
--- my-default-installer/run-all.sh
+++ my-default-installer/run-all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-00-fundamentals.sh
-40-media.sh
-50-graphics.sh
-60-dev.sh
+./00-fundamentals.sh
+./40-media.sh
+./50-graphics.sh
+./60-dev.sh
+./65-repos.sh
