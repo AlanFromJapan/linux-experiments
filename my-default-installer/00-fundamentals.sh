9a110b1904a035f884f3afe23441e10198be4618
Alan <alanfromjapan@gmail.com>
2021-10-08 21:47:31 +0900

    Continued adding aliases, some AVR dev packages too

--- my-default-installer/00-fundamentals.sh
+++ my-default-installer/00-fundamentals.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#alias als && echo "Alias already registered : skipping." || cat my-cmd-aliases >> ~/.bashrc
+
 sudo apt install emacs-nox git --yes
 
-sudo apt install keepassxc htop --yes
+sudo apt install keepassxc htop timeshift --yes
 
--- my-default-installer/60-dev.sh
+++ my-default-installer/60-dev.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo apt install python3 gcc --yes
+
+sudo apt install  gcc-avr binutils binutils-avr avrdude avr-libc --yes
