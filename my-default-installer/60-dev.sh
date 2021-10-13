fdf379c317d95ee4db7dd08292611bf327972ab5
Alan <alanfromjapan@gmail.com>
2021-10-13 22:41:28 +0900

    Added repo and packages for the 64MB gameboy smart cartridge

--- my-default-installer/60-dev.sh
+++ my-default-installer/60-dev.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#basics
 sudo apt install python3 gcc --yes
 
-sudo apt install  gcc-avr binutils binutils-avr avrdude avr-libc --yes
+#AVR related
+sudo apt install  gcc-avr binutils binutils-avr avrdude avr-libc  --yes
+
+#GB dev related
+sudo apt install  sdcc visualboyadvance --yes
+#For the 64MB card "ems-flasher"
+sudo apt install  pkg-config libusb-1.0-0-dev --yes
+
+
+
--- my-default-installer/65-repos.sh
+++ my-default-installer/65-repos.sh
@@ -2,9 +2,14 @@
 
 mkdir -p ~/Git
 
+#my stuffs
 git clone https://github.com/AlanFromJapan/alanarduinotools.git ~/Git/alanarduinotools
 git clone https://github.com/AlanFromJapan/alanWebSites.git ~/Git/alanWebSites
 git clone https://github.com/AlanFromJapan/Gameboy.git ~/Git/Gameboy
 git clone https://github.com/AlanFromJapan/pcb-design.git ~/Git/pcb-design
 git clone https://github.com/AlanFromJapan/linux-experiments.git ~/Git/linux-experiments
+
+#gameboy dev related
+git clone https://github.com/andreasjhkarlsson/gbdk-n.git ~/Git/gbdk-n
+git clone https://github.com/mikeryan/ems-flasher ~/Git/ems-flasher
 
