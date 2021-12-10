f2a4d7c45eb06d7da7378fb2536100c3271b1b04
Alan <alanfromjapan@gmail.com>
2021-12-10 18:25:58 +0900

    made password optional from command line

--- helper-scripts/veracrypt-newfile.sh
+++ helper-scripts/veracrypt-newfile.sh
@@ -5,15 +5,25 @@
 
 echo "Creates an empty veracrypt file"
 
-if [[ $# -ne 3 ]]; then
+PWD_PARAM=""
+
+if [[ $# -lt 2 ]]; then
     echo "Illegal number of parameters" >&2
-    echo "Usage: $0 <path to file> <size in MB> <password>"
+    echo "Usage: $0 <path to file> <size in MB> [<password>]"
     exit 2
 fi
 
+if [[ $# -eq 3 ]]; then
+    echo "Password specified on the command line (you are a trusting guy aren't you?). Using non-interactive mode."
+    PWD_PARAM="--password=$3 --non-interactive"
+else
+    echo "No password as parameter: VC will ask you."
+    PWD_PARAM=
+fi
+
 #This one works if you have the CONSOLE version
-#veracrypt --create $1 --password=$3 --hash=sha512 --encryption=Twofish --filesystem=FAT --size=$2M --volume-type=Normal --non-interactive --random-source=/dev/random
+#veracrypt --create $1  --hash=sha512 --encryption=Twofish --filesystem=FAT --size=$2M --volume-type=Normal $PWD_PARAM --random-source=/dev/random
 
 #This one works if you have the GUI version
-veracrypt --text --create $1 --password=$3 --hash=sha512 --encryption=Twofish --filesystem=FAT --size=$2M --volume-type=Normal --non-interactive --random-source=/dev/random
+veracrypt --text --create $1 --hash=sha512 --encryption=Twofish --filesystem=FAT --size=$2M --volume-type=Normal $PWD_PARAM --random-source=/dev/random
 
