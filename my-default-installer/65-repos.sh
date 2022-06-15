7c3ff496289b846d84d6d4576278380b765d9ebe
alanfromjapan <alanfromjapan@gmail.com>
2022-06-15 21:47:24 +0900

    Added missing ipcampy.nocv project

--- my-default-installer/65-repos.sh
+++ my-default-installer/65-repos.sh
@@ -10,6 +10,7 @@ git clone https://github.com/AlanFromJapan/pcb-design.git ~/Git/pcb-design
 git clone https://github.com/AlanFromJapan/linux-experiments.git ~/Git/linux-experiments
 git clone https://github.com/AlanFromJapan/MTGA-experiments.git ~/Git/MTGA-experiments
 git clone https://github.com/AlanFromJapan/ipcampy.git ~/Git/ipcampy
+git clone -b master https://github.com/AlanFromJapan/ipcampy.nocv.git ~/Git/ipcampy.nocv
 #git clone https://github.com/AlanFromJapan/terraforming.git ~/Git/terraforming
 
 #gameboy dev related
