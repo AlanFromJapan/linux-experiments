5784ef0a7f0d2ad3631cd56768d847cb8e8e2354
AlanFromJapan <alanfromjapan@gmail.com>
2023-02-17 00:02:50 +0900

    Added security file

---
+++ my-default-installer/90-security.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#now go "Do nothing" mode
+echo "#################################################################"
+echo "##                                                             ##"
+echo "##  Entering do nothing mode                                   ##"
+echo "##  Fundamentals are installed, but you have some manual work, ##"
+echo "##  future me. Just do as it says.                             ##"
+echo "##                                                             ##"
+echo "#################################################################"
+
+echo "Install VeraCrypt -> https://www.veracrypt.fr/en/Downloads.html"
+
+read -n1 -s -r -p $'Press any key to continue...\n' key
+
+echo "Configure"
+
