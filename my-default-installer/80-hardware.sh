8268ead8b3b6d780b728687cc81ea5306a447163
AlanFromJapan <alanfromjapan@gmail.com>
2023-02-16 23:29:09 +0900

    Added info for the old Buffalo NAS

--- my-default-installer/80-hardware.sh
+++ my-default-installer/80-hardware.sh
@@ -13,4 +13,25 @@ echo "CANON PRINTER: download the drivers and install -> https://cweb.canon.jp/d
 
 read -n1 -s -r -p $'Press any key to continue...\n' key
 
+
+
+
+
+
+echo "BUFFALO NAS: 1) need some more settings since Ubuntu 20.04 -> see https://www.logikalsolutions.com/wordpress/information-technology/make-ubuntu-work-with-your-network-drives/"
+
+sudo apt install samba-common samba-common-bin samba-dsdb-modules samba-libs --yes
+
+echo "2) Now you need to edit your /etc/samba/smb.conf and add 'client min protocol = NT1' somewhere. Do it."
+
+read -n1 -s -r -p $'Press any key to continue...\n' key
+
+echo "3) Reboot, now it should work "
+
+read -n1 -s -r -p $'Press any key to continue...\n' key
+
+
+
+
+
 echo "Finished!"
