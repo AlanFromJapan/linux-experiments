7cc3b3b858a0a5fc92d5d23f3dfc92dbcecf4365
AlanFromJapan <alanfromjapan@gmail.com>
2023-04-11 10:22:01 +0900

    Added command to set default screen for login

--- my-default-installer/99-final-touch.sh
+++ my-default-installer/99-final-touch.sh
@@ -13,5 +13,19 @@ echo "Install Ghostery or you favorite addblocker on Firefox"
 
 read -n1 -s -r -p $'Press any key to continue...\n' key
 
-echo "Configure"
 
+
+
+
+#https://askubuntu.com/questions/1043337/is-there-to-make-the-login-screen-appear-on-the-external-display-in-18-04
+echo "Configure your displays the way you want (primary, left/right etc) and then save it to GDM user so boot uses the right PRIMARY SCREEN"
+
+echo "Run this command manually once you're done:"
+echo "sudo cp ~/.config/monitors.xml ~gdm/.config/monitors.xml; sudo chown gdm:gdm ~gdm/.config/monitors.xml"
+
+read -n1 -s -r -p $'Press any key to continue...\n' key
+
+
+
+
+echo "Finished!"
