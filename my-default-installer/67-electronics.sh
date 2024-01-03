9b6e981ec23088760d8f603aa82aadc04aeb6fe9
AlanFromJapan <alanfromjapan@gmail.com>
2024-01-03 21:38:11 +0900

    Added 67 for electronics dev

---
+++ my-default-installer/67-electronics.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "#################################################################"
+echo "##                                                             ##"
+echo "##  Setup of the tools for electronics dev                     ##"
+echo "##                                                             ##"
+echo "#################################################################"
+
+sudo apt install gerbv --yes
+
+#now go "Do nothing" mode
+echo "#################################################################"
+echo "##                                                             ##"
+echo "##  Entering do nothing mode                                   ##"
+echo "##  Fundamentals are installed, but you have some manual work, ##"
+echo "##  future me. Just do as it says.                             ##"
+echo "##                                                             ##"
+echo "#################################################################"
+
+read -n1 -s -r -p $'Press any key to continue...\n' key
+
+echo "Download the KiCad installer from https://kicad.org/download/ubuntu/"
+echo "and install it with 'sudo dpkg -i <installer>'"
+echo "**or**"
+echo "Install Eagle (find the installer in my backups)"
+
+read -n1 -s -r -p $'Press any key to continue...\n' key
