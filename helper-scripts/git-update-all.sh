23a185d0ca7df461dd109a5aece7f77b8024ee2f
AlanFromJapan <alanfromjapan@gmail.com>
2024-03-25 22:08:29 +0900

    Added colors!

--- helper-scripts/git-update-all.sh
+++ helper-scripts/git-update-all.sh
@@ -2,13 +2,26 @@
 #---------------------------------
 # Do a git pull on all sub directories
 
-for f in "$1"/*; do
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+CYAN='\033[0;36m'
+
+echo -e "${CYAN}############################################################################################################## "
+echo GIT PULL updating all sub-folders
+echo -e "##############################################################################################################$NC"
+for d in ./*/ ;
+do
 	#directories only
-	if [ -d "$f" ]; then
-		echo "Updating folder $f ..."
-		cd $f
+	if [ -d "$d" ]; then
+		cd $d
+		
+		echo -e "GIT PULL Updating ${RED}`pwd`$NC ..."
 		git pull
+		echo ""
+		
 		cd ..
 	fi
 done
+
+echo -e "$CYAN ... completed! $NC"
 
