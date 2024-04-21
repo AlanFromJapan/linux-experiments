5179b010d9123eabed6a350770a8f878d46548c6
AlanFromJapan <alanfromjapan@gmail.com>
2024-04-21 21:29:53 +0900

    Added pages stats (top)

--- task-specific-scripts/website-log-analyzer.py
+++ task-specific-scripts/website-log-analyzer.py
@@ -35,7 +35,7 @@ class Visitor:
     
 
 def analyze_logs(log_file):
-    visitor = {}
+    visitors = {}
 
     with open(log_file, 'r') as file:
         for line in file:
@@ -43,21 +43,33 @@ def analyze_logs(log_file):
             if reNormal.match(l):
                 ip, page, status = reNormal.match(l).groups()
 
-                if ip not in visitor:
-                    visitor[ip] = Visitor(ip)
-                v = visitor[ip]
+                if ip not in visitors:
+                    visitors[ip] = Visitor(ip)
+                v = visitors[ip]
 
                 v.add_page(page, status)
             elif reBadReq.match(l):
                 ip, page = reBadReq.match(l).groups()
 
-                if ip not in visitor:
-                    visitor[ip] = Visitor(ip)
-                v = visitor[ip]
+                if ip not in visitors:
+                    visitors[ip] = Visitor(ip)
+                v = visitors[ip]
 
                 v.add_bad_req(page, -1)
 
-    return visitor              
+    return visitors              
+
+
+def sort_pages(v):
+    pages = {}
+    for visitor in v.values():
+        for page in visitor.pages:
+            if page[0] not in pages:
+                pages[page[0]] = 1
+            else:
+                pages[page[0]] += 1
+    return sorted(pages.items(), key=lambda x: x[1], reverse=True)
+
 
 if len(sys.argv) != 2:
     print("Usage: python website-log-analyzer.py <log file>")
@@ -72,3 +84,9 @@ print("Average number of hits per visitor: ", sum([v.uniqueHits() for v in V.val
 print ("Number of robots: ", len([v for v in V.values() if v.isRobot]))
 print ("Number of bad requests: ", len([v for v in V.values() if v.isBadReq]))
 print ("Number of robots.txt request: ", len([v for v in V.values() if v.isRobot and "robots.txt" in v.pages[0][0]]))
+
+sortedPages =  sort_pages(V)
+print("Top 20 REQUESTS:")
+print(sortedPages[:20])
+print("Top 20 HTML PAGES:")
+print([x for x in sortedPages if ".html" in x[0]][:20])
